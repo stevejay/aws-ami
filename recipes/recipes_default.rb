6c55dee57eb4fb52df465dd5690d3ad0a04b122a
Stephen Johns <steve@stevejay.net>
2015-06-04 21:50:46 +0100

    messing around

---
+++ recipes/recipes_default.rb
@@ -0,0 +1,18 @@
+include_recipe 'apt'
+include_recipe 'nginx'
+ 
+directory '/var/www/nginx-default' do
+  owner 'www-data'
+  group 'www-data'
+  mode '0755'
+  recursive true
+  action :create
+end
+ 
+file '/var/www/nginx-default/index.html' do
+  owner 'www-data'
+  group 'www-data'
+  mode '0755'
+  content 'Hello World from Packer!'
+  action :create
+end

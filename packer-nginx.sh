2196bd28a082d3e9ebfd6c0d55f4e051af9da74a
Stephen Johns <steve@stevejay.net>
2015-06-05 21:01:41 +0100

    messing around

--- packer-nginx.sh
+++ packer-nginx.sh
@@ -27,24 +27,24 @@ echo "packer: tweaking tcp"
 sudo sysctl -w net.ipv4.tcp_slow_start_after_idle=0
 sudo sysctl -w net.ipv4.tcp_window_scaling=1
 
-echo "packer: ipv4 forwarding"
-cp /etc/sysctl.conf /tmp/
-echo "net.ipv4.ip_forward = 1" >> /tmp/sysctl.conf
-sudo cp /tmp/sysctl.conf /etc/
-sudo sysctl -p /etc/sysctl.conf
+#echo "packer: ipv4 forwarding"
+#cp /etc/sysctl.conf /tmp/
+#echo "net.ipv4.ip_forward = 1" >> /tmp/sysctl.conf
+#sudo cp /tmp/sysctl.conf /etc/
+#sudo sysctl -p /etc/sysctl.conf
 
-echo "packer: forward port 80 to 8080"
-sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
-sudo iptables -A INPUT -p tcp -m tcp --sport 80 -j ACCEPT
-sudo iptables -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT
-sudo iptables-save > /tmp/iptables-store.conf
-sudo mv /tmp/iptables-store.conf /etc/iptables-store.conf
+#echo "packer: forward port 80 to 8080"
+#sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+#sudo iptables -A INPUT -p tcp -m tcp --sport 80 -j ACCEPT
+#sudo iptables -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT
+#sudo iptables-save > /tmp/iptables-store.conf
+#sudo mv /tmp/iptables-store.conf /etc/iptables-store.conf
 
-echo "packer: remember port forwarding rule across reboots"
-echo "#!/bin/sh" > /tmp/iptables-ifupd
-echo "iptables-restore < /etc/iptables-store.conf" >> /tmp/iptables-ifupd
-chmod +x /tmp/iptables-ifupd
-sudo mv /tmp/iptables-ifupd /etc/network/if-up.d/iptables
+#echo "packer: remember port forwarding rule across reboots"
+#echo "#!/bin/sh" > /tmp/iptables-ifupd
+#echo "iptables-restore < /etc/iptables-store.conf" >> /tmp/iptables-ifupd
+#chmod +x /tmp/iptables-ifupd
+#sudo mv /tmp/iptables-ifupd /etc/network/if-up.d/iptables
 
 echo "packer: precaching server dependencies"
 #mkdir -p ~/app/precache
@@ -54,3 +54,9 @@ echo "packer: precaching server dependencies"
 
 echo "packer: installing nginx at system startup"
 sudo update-rc.d nginx defaults
+
+
+
+
+
+sudo service nginx reload

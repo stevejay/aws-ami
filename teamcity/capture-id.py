6af9a50b8fffdcbf2c2578d5a2f192464e063454
Stephen Johns <steve@stevejay.net>
2015-07-12 00:18:22 +0100

    stuff

--- teamcity/capture-id.py
+++ teamcity/capture-id.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python
 
-import argparse, logging, sys
+import argparse, logging, sys, re
 
 logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
 
 parser = argparse.ArgumentParser(add_help=True, description='Gets AMI ID from packer log file')
 
 parser.add_argument('--infile', '-i', metavar='in-file', type=argparse.FileType('rt'), dest='in_file', required=True, help='The packer log file')
-parser.add_argument('--regex', '-r', dest="bucket", required=True, help='The regex to find the ID')
-parser.add_argument('--parameter', '-p', dest="bucket", required=True, help='The TeamCity parameter name to set')
+parser.add_argument('--regex', '-r', dest="regex", required=True, help='The regex to find the ID')
+parser.add_argument('--parameter', '-p', dest="parameter", required=True, help='The TeamCity parameter name to set')
 
 try:
 	results = parser.parse_args()
 	
 	with results.in_file as source_file:
-		boto3.resource('s3').Bucket(results.bucket).put_object(Key=results.out_file, Body=source_file)
+		content = source_file.read()
+		match = re.search(results.regex, content)
+		if not match: raise Exception('no match found for regex')
+		print "##teamcity[setParameter name='" + results.parameter + "' value='" + match.group(1) + "']"
 except:
-	logging.info('Failed to upload to s3', exc_info=False)
+	logging.error('Failed to capture ID', exc_info=True)
 	exit(1)
